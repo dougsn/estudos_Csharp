a0d79cc96d6ab1cac414f69f85e72f2260aa6a30
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-09-12 16:16:39 -0300

    First commit

---
+++ Classe_Csharp/Classe_Csharp/Program.cs
@@ -0,0 +1,67 @@
+﻿
+
+using Classe_Csharp;
+using System.Globalization;
+
+class Program {
+
+    static void Main(string[] args) {
+
+        /*        Triangulo x, y;
+
+                x = new Triangulo();
+                y = new Triangulo();
+
+                Console.WriteLine("Entre com as medidas do triângulo X: ");
+                x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Entre com as medidas do triângulo Y: ");
+                y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double areaX = x.Area();
+
+                double areaY = y.Area();
+
+
+                Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+                Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+                if (areaX > areaY) {
+                    Console.WriteLine("Maior área: X");
+                } else {
+                    Console.WriteLine("Maior área: Y");
+                }*/
+
+        Produto p = new Produto();
+
+        Console.WriteLine("Entre os dados do produto: ");
+        Console.Write("Nome: ");
+        p.Nome = Console.ReadLine();
+        Console.Write("Preço: ");
+        p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        p.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("Dados do produto: " + p);
+        Console.WriteLine();
+
+        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+        int qte = int.Parse(Console.ReadLine());
+        p.AdicionarProdutos(qte);
+
+        Console.WriteLine("Dados atualizados: " + p);
+
+
+        Console.Write("Digite o número de produtos a ser removido do estoque: ");
+
+        qte = int.Parse(Console.ReadLine());
+        p.RemoverProdutos(qte);
+        Console.WriteLine("Dados atualizados: " + p);
+
+    }
+}
---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace EntradaDeDados {
+    class Program {
+        static void Main(string[] agrs) {
+
+            // string frase = Console.ReadLine();
+            // string x = Console.ReadLine();
+            // string y = Console.ReadLine();
+            // string z = Console.ReadLine();
+
+            // string[] v = Console.ReadLine().Split(' ');
+            // string a = v[0];
+            // string b = v[1];
+            // string c = v[2];
+
+            //Console.WriteLine("Você digitou: ");
+            //Console.WriteLine(frase);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            // Entrada de Dados parte 2
+
+            int n1 = int.Parse(Console.ReadLine()); // Convertendo de inteiro para String
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // o CultureInfo, transforma a ,(vírgula) em .(ponto)
+
+            string[] s = Console.ReadLine().Split(' ');
+            string nome = s[0];
+            char sexo = char.Parse(s[1]);
+            int idade = int.Parse(s[2]);
+            double altura = double.Parse(s[3], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
