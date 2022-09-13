033455abc37c8486fd3f1b45d2da0dfd76dd8c03
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-09-13 16:33:20 -0300

    Construtores_Sobrecarga_Encapsulamento em c#

---
+++ Construtoes_Sobrecarga_Encapsulamento/Construtoes_Sobrecarga_Encapsulamento/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtoes_Sobrecarga_Encapsulamento {
+    internal class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += 1;
+        }
+
+        public override string ToString() {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)
+                ;
+        }
+
+
+    }
+}
