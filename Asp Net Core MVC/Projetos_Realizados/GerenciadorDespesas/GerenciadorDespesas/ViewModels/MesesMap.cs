88fae9da00e71329c427aa495d8e9daff573ea80
dougsn <douglassnascimet@gmail.com>
2022-10-03 21:16:48 -0300

    Finalizando fráfico de total de gasto por mẽs

---
+++ Asp Net Core MVC/Projetos_Realizados/GerenciadorDespesas/GerenciadorDespesas/ViewModels/MesesMap.cs
@@ -0,0 +1,12 @@
+﻿using GerenciadorDespesas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GerenciadorDespesas.Mapeamento
+{
+    public class GastosTotaisMesViewModel   
+    {
+        public double ValorTotalGasto { get; set; }
+        public double Salario { get; set; }
+    }
+}
