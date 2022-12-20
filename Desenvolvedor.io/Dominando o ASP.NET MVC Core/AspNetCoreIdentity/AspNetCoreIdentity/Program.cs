a72f1b12b148316f85284db4bcf156493a6acfff
Douglas <douglassnascimet@gmail.com>
2022-12-20 16:23:28 -0300

    Finalizando Configuração do Identity

--- Desenvolvedor.io/Dominando o ASP.NET MVC Core/AspNetCoreIdentity/AspNetCoreIdentity/Program.cs
+++ Desenvolvedor.io/Dominando o ASP.NET MVC Core/AspNetCoreIdentity/AspNetCoreIdentity/Program.cs
@@ -1,27 +1,75 @@
+using AspNetCoreIdentity.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+// *** Configurando serviços no container ***
+
+// Politica de cookies (LGPD)
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+// Adicionando suporte ao contexto do Identity via EF
+builder.Services.AddDbContext<AspNetCoreIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+// Adicionando configuração padrão do Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
+// Adicionando Autorizações personalizadas por policies
+
+// Adicionando MVC no pipeline
 builder.Services.AddControllersWithViews();
+
+// Adicionando suporte a componentes Razor (ex. Telas do Identity)
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+// *** Configurando o resquest dos serviços no pipeline *** 
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/erro/500");
+    app.UseStatusCodePagesWithRedirects("/erro/{0}");
     app.UseHsts();
 }
 
+// Redirecionamento para HTTPs
 app.UseHttpsRedirection();
+
+// Uso de arquivos estáticos (ex. CSS, JS)
 app.UseStaticFiles();
 
+// Adicionando suporte a rota
 app.UseRouting();
 
+// Autenticacao e autorização (Identity)
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+// Rota padrão
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Mapeando componentes Razor Pages (ex: Identity)
+app.MapRazorPages();
 
 app.Run();
