5af29e58cd8e3000709840a59a524fd554e3d035
Douglas <douglassnascimet@gmail.com>
2022-12-27 15:51:33 -0300

    Iniciando a AppMvcBasica, finalizando a primeira aula

---
+++ Desenvolvedor.io/Dominando o ASP.NET MVC Core/AppMvcBasica/AppMvcBasica/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMvcBasica.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        /* Realizando o mapeamento das classes para o banco de dados.*/
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Endereco> Enderecos{ get; set; }
+
+    }
+}
